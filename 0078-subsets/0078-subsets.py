8a3258fb75ed67bc14efe2116e3946ea1ed4febd
jessmit1217 <119490439+merry1214@users.noreply.github.com>
2025-08-27 21:53:23 +0300

    Time: 3 ms (10.73%), Space: 17.9 MB (65.73%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -2,7 +2,7 @@ class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         n = len(nums)
         res = []
-        for mask in range(1 << n):  # 2^n subsets
+        for mask in range(1 << n):  
             subset = []
             for i in range(n):
                 if mask & (1 << i):
