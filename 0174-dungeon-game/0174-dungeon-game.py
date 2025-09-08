2ddc7c5c6410288b57cc5987d2acf0f16ccf5f83
Meron Bahru <119490439+merry1214@users.noreply.github.com>
2025-09-08 20:51:51 +0300

    Time: 7 ms (58.27%), Space: 18.8 MB (59.71%) - LeetHub

--- 0174-dungeon-game/0174-dungeon-game.py
+++ 0174-dungeon-game/0174-dungeon-game.py
@@ -2,7 +2,7 @@ class Solution:
     def calculateMinimumHP(self, dungeon: List[List[int]]) -> int:
         m, n = len(dungeon), len(dungeon[0])
         dp = [[float('inf')] * (n + 1) for _ in range(m + 1)]
-        dp[m][n-1] = dp[m-1][n] = 1  # base case for princess cell
+        dp[m][n-1] = dp[m-1][n] = 1  
 
         for i in range(m-1, -1, -1):
             for j in range(n-1, -1, -1):
