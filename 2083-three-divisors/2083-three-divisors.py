ef800655985696c65ad217b767fbd05c609534fa
Meron Bahru <119490439+merry1214@users.noreply.github.com>
2025-09-14 22:47:56 +0300

    Time: 0 ms (100%), Space: 17.9 MB (38.52%) - LeetHub

---
+++ 2083-three-divisors/2083-three-divisors.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isThree(self, n: int) -> bool:
+        root = int(math.sqrt(n))
+        if root * root != n:
+            return False  
+        if root < 2:
+            return False
+        
+        for i in range(2, int(math.sqrt(root)) + 1):
+            if root % i == 0:
+                return False
+        return True
+        
